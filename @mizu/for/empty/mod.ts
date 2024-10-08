94823bd01c16e24faed54deeb6a6d4caf5a6fe79
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-10-07 22:53:14 -0400

    feat(for): add `*empty` directive

---
+++ @mizu/for/empty/mod.ts
@@ -0,0 +1,55 @@
+// Imports
+import { type Cache, type Directive, Phase } from "@mizu/mizu/core/engine"
+import { _for, _id } from "@mizu/for"
+import { _if } from "@mizu/if"
+export type * from "@mizu/mizu/core/engine"
+
+/** Typings. */
+export const typings = {
+  modifiers: {
+    not: { type: Boolean },
+  },
+} as const
+
+/** `*empty` directive. */
+export const _empty = {
+  name: "*empty",
+  phase: Phase.TOGGLE,
+  typings,
+  execute(renderer, element, { attributes: [attribute] }) {
+    const parsed = renderer.parseAttribute(attribute, this.typings, { modifiers: true })
+    const cache = renderer.cache<Cache<typeof _for>>(_for.name)
+    const seen = [] as HTMLElement[]
+    let previous = element.previousSibling as HTMLElement
+    while (previous) {
+      // Break on non-empty text nodes
+      if ((previous.nodeType === renderer.window.Node.TEXT_NODE) && (previous.textContent?.trim())) {
+        break
+      }
+
+      // Break on element node that was not created by a for directive or without an empty directive
+      if ((previous.nodeType === renderer.window.Node.ELEMENT_NODE) && (!renderer.getAttributes(previous, [_empty.name, _id.name], { first: true }))) {
+        seen.push(previous)
+      }
+
+      // Execute directive on first for loop found
+      if ((previous.nodeType === renderer.window.Node.COMMENT_NODE) && (cache?.has(previous))) {
+        const items = [...cache.get(previous)!.items.values()]
+        const $generated = items.length
+        if (seen.some((item) => !items.includes(item))) {
+          break
+        }
+        return {
+          ..._if.execute(renderer, element, { ...arguments[2], _directive: { directive: this.name, expression: attribute.value, value: `!${parsed.modifiers.not ? "!" : ""}${$generated}` } }),
+          state: { $generated },
+        }
+      }
+      previous = previous.previousSibling as HTMLElement
+    }
+    renderer.warn(`[${this.name}] must be immediately preceded by a [${_for.name}] or [${_empty.name}] directive, ignoring`, element)
+    return { state: { $generated: NaN } }
+  },
+} as Directive<null, typeof typings> & { name: string }
+
+/** Default exports. */
+export default [_for, _empty]
