3775db70c73e96c8015819f5f80bf49694178104
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-10-07 22:49:10 -0400

    feat(once): add `*once` directive

---
+++ @mizu/once/mod.ts
@@ -0,0 +1,31 @@
+// Imports
+import { type Cache, type Directive, Phase } from "@mizu/mizu/core/engine"
+export type * from "@mizu/mizu/core/engine"
+
+/** `*once` directive. */
+export const _once = {
+  name: "*once",
+  phase: Phase.POSTPROCESSING,
+  init(renderer) {
+    renderer.cache<Cache<typeof _once>>(this.name, new WeakSet())
+  },
+  setup(_, element, { cache }) {
+    if (cache.has(element)) {
+      return false
+    }
+  },
+  cleanup(renderer, element, { cache }) {
+    let target = element
+    if ((renderer.isComment(element)) && (renderer.cache("*").has(element))) {
+      target = renderer.cache("*").get(element)!
+    }
+    const attribute = renderer.getAttributes(target, this.name, { first: true })
+    if (!attribute) {
+      return
+    }
+    cache.add(element)
+  },
+} as Directive<WeakSet<HTMLElement | Comment>>
+
+/** Default exports. */
+export default _once
