911d3c5c27dfd95b043ac2fc4735a2ef43f7851e
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-10-07 22:54:00 -0400

    feat(show): add `*show` directive

---
+++ @mizu/show/mod.ts
@@ -0,0 +1,26 @@
+// Imports
+import { type Directive, Phase } from "@mizu/mizu/core/engine"
+export type * from "@mizu/mizu/core/engine"
+
+/** `*show` directive. */
+export const _show = {
+  name: "*show",
+  phase: Phase.DISPLAY,
+  async execute(renderer, element, { attributes: [attribute], ...options }) {
+    if (renderer.isComment(element)) {
+      return
+    }
+    const result = Boolean(await renderer.evaluate(element, attribute.value, options))
+    if (result) {
+      element.style.removeProperty("display")
+      if (!element.style.length) {
+        element.removeAttribute("style")
+      }
+    } else {
+      element.style.setProperty("display", "none", "important")
+    }
+  },
+} as Directive
+
+/** Default exports. */
+export default _show
