ec4bcb516abd48d53e6289db29e1978a119e4b38
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-10-07 22:49:15 -0400

    feat(if): add `*if` directive (#5)

---
+++ @mizu/if/mod.ts
@@ -0,0 +1,33 @@
+// Imports
+import { type Directive, Phase } from "@mizu/mizu/core/engine"
+export type * from "@mizu/mizu/core/engine"
+
+/**
+ * `*if` directive.
+ *
+ * @internal `_directive.value` Force the directive to use specified value rather than the attribute value.
+ * @internal `_directive.expression` Set the original expression for the directive if element is commented out.
+ * @internal `_directive.directive` Set the original directive name for the directive if element is commented out.
+ */
+export const _if = {
+  name: "*if",
+  phase: Phase.TOGGLE,
+  async execute(renderer, element, { attributes: [attribute], ...options }) {
+    const result = Boolean(await renderer.evaluate(element, arguments[2]._directive?.value ?? attribute.value, options))
+    switch (true) {
+      // Switch comment to element if truthy
+      case result && (renderer.isComment(element)) && (renderer.cache("*").has(element)): {
+        const original = renderer.uncomment(element)
+        return { element: original }
+      }
+      // Switch element to comment if falsy
+      case (!result) && (renderer.isHtmlElement(element)): {
+        const comment = renderer.comment(element, { expression: attribute.value, directive: _if.name, ...arguments[2]._directive })
+        return { element: comment, final: true }
+      }
+    }
+  },
+} as Directive & { execute: NonNullable<Directive["execute"]> }
+
+/** Default exports. */
+export default _if
