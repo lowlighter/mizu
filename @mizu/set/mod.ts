0ed54346dc3f88451bf5d8221e1994fe67df5b2c
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-10-07 23:01:29 -0400

    feat(set): add `*set` directive

---
+++ @mizu/set/mod.ts
@@ -0,0 +1,26 @@
+// Imports
+import { type Cache, type Context, type Directive, type Nullable, Phase } from "@mizu/mizu/core/engine"
+export type * from "@mizu/mizu/core/engine"
+
+/** `*set` directive. */
+export const _set = {
+  name: "*set",
+  phase: Phase.CONTEXT,
+  init(renderer) {
+    renderer.cache<Cache<typeof _set>>(this.name, new WeakMap())
+  },
+  async execute(renderer, element, { attributes: [attribute], cache, ...options }) {
+    if (!cache.has(element)) {
+      const context = await renderer.evaluate(element, attribute.value, options)
+      if (typeof context !== "object") {
+        renderer.warn(`[${this.name}] expects an object but got ${typeof context}, ignoring`, element)
+        return
+      }
+      cache.set(element, context ? options.context.with(context as Record<PropertyKey, unknown>) : null)
+    }
+    return { context: cache.get(element) }
+  },
+} as Directive<WeakMap<HTMLElement | Comment, Nullable<Context>>>
+
+/** Default exports. */
+export default _set
