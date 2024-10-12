effaf048d14dbf5951075a519130fc006de63bf2
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-10-12 13:17:13 -0400

    feat(mustache): add `*mustache` directive (#32)

---
+++ @mizu/mustache/capture.ts
@@ -0,0 +1,68 @@
+/** Tokens. */
+const tokens = {
+  "(": ")",
+  "[": "]",
+  "{": "}",
+  '"': '"',
+  "'": "'",
+  "`": "`",
+} as Record<PropertyKey, string>
+
+/**
+ * Capture content between delimiters.
+ *
+ * @example
+ * ```ts
+ * const { captured, match } = capture("foo {{ bar }} baz")!
+ * console.assert(captured === "bar")
+ * console.assert(match === "{{ bar }}")
+ * ```
+ *
+ * @author Simon Lecoq (lowlighter)
+ * @license MIT
+ */
+export function capture(string: string, offset = 0) {
+  const stack = []
+  let a = NaN
+  let d = 2
+  let quoted = false
+  for (let i = offset; i < string.length; i++) {
+    // Start capturing upon meeting mustache opening
+    if (Number.isNaN(a)) {
+      if ((string[i] === "{") && (string[i + 1] === "{")) {
+        if (string[i + 2] === "{") {
+          d = 3
+        }
+        a = i
+        i += d - 1
+      }
+      continue
+    }
+    // Close capturing on mustache closing (stack must be empty)
+    if ((!stack.length) && (string[i] === "}") && (string[i + 1] === "}") && ((d === 2) || (string[i + 2] === "}"))) {
+      return {
+        a,
+        b: i + d,
+        match: string.slice(a, i + d),
+        captured: string.slice(a + d, i).trim(),
+        triple: d === 3,
+      }
+    }
+    // Close group on closing token
+    if (string[i] === tokens[stack.at(-1)!]) {
+      stack.pop()
+      continue
+    }
+    // Open group on opening token
+    if ((!quoted) && (string[i] in tokens)) {
+      stack.push(string[i])
+      quoted = ["'", '"', "`"].includes(string[i])
+      continue
+    }
+  }
+  // Throw on mustache unclosed
+  if (!Number.isNaN(a)) {
+    throw new SyntaxError(`Unclosed expression, unterminated expression at: ${a}\n${string.slice(a)}`)
+  }
+  return null
+}
---
+++ @mizu/mustache/capture_test.ts
@@ -0,0 +1,41 @@
+import { expect, test } from "@libs/testing"
+import { capture } from "./capture.ts"
+
+test()("`capture()` captures content between double delimiters", () => {
+  expect(capture("foo {{ bar }} baz")).toMatchObject({
+    captured: "bar",
+    match: "{{ bar }}",
+    a: "foo ".length,
+    b: "foo {{ bar }}".length,
+    triple: false,
+  })
+})
+
+test()("`capture()` captures content between triple delimiters", () => {
+  expect(capture("foo {{{ bar }}} baz")).toMatchObject({
+    captured: "bar",
+    match: "{{{ bar }}}",
+    a: "foo ".length,
+    b: "foo {{{ bar }}}".length,
+    triple: true,
+  })
+})
+
+test()("`capture()` handles nested delimiters", () => {
+  for (const d of ["{{", "{{{"]) {
+    const b = { "{{": "}}", "{{{": "}}}" }[d]
+    for (const q of ["'", '"', "`"]) {
+      expect(capture(`foo ${d} ${q}${d} bar ${b}${q} ${b} baz`)).toMatchObject({ captured: `${q}${d} bar ${b}${q}` })
+      expect(capture(`foo ${d} ${q}bar ${b}${q} ${b} baz`)).toMatchObject({ captured: `${q}bar ${b}${q}` })
+      expect(capture(`foo ${d} ${q}${d} bar${q} ${b} baz`)).toMatchObject({ captured: `${q}${d} bar${q}` })
+    }
+    expect(capture(`${d} [{}, {}] ${b}`)).toMatchObject({ captured: "[{}, {}]" })
+    expect(capture(`${d} ({}, {}) ${b}`)).toMatchObject({ captured: "({}, {})" })
+    expect(capture(`${d} {foo:{}}[bar] ${b}`)).toMatchObject({ captured: "{foo:{}}[bar]" })
+    expect(capture(`${d} (() => {{ 1 }}) ${b}`)).toMatchObject({ captured: "(() => {{ 1 }})" })
+  }
+})
+
+test()("`capture()` throws `new SyntaxError()` on unclosed expressions", () => {
+  expect(() => capture("foo {{ bar baz"))._toThrow(SyntaxError, "Unclosed expression")
+})
