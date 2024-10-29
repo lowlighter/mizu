288d3adf4497627b3089844e0c6e97bc906756ae
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-10-28 21:32:43 -0400

    chore: update issue templates

---
+++ .github/tools/fill_issue_templates.ts
@@ -0,0 +1,22 @@
+// Imports
+import type { rw } from "@libs/typing/types"
+import { parse, stringify } from "@std/yaml"
+import { expandGlob } from "@std/fs"
+import { Logger } from "@libs/logger"
+import { command } from "@libs/run/command"
+const log = new Logger()
+
+if (import.meta.main) {
+  const scopes = [...await Array.fromAsync(Deno.readDir("@mizu"))].map(({ name }) => name)
+  scopes.push("www", "repo")
+  for await (const { path, name } of expandGlob(".github/ISSUE_TEMPLATE/*.yml")) {
+    if (name === "config.yml") {
+      continue
+    }
+    const parsed = parse(await Deno.readTextFile(path)) as rw
+    parsed.body.find((filed: rw) => filed.id === "scope").attributes.options = scopes
+    await Deno.writeTextFile(path, stringify(parsed))
+    await command("deno", ["fmt", path], { stdout: null, stderr: null })
+    log.with({ path }).ok("done")
+  }
+}
