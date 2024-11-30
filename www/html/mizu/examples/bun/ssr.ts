58f328eb4b1d04e3a5073f3099c07d0564d22300
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-11-30 03:29:02 -0500

    doc(www): add node and bun examples (#46)

---
+++ www/html/mizu/examples/bun/ssr.ts
@@ -0,0 +1,13 @@
+// Server-Side Rendering (SSR) with Mizu
+import Mizu from "@mizu/render/server"
+// @ts-expect-error: bun
+Bun.serve({
+  port: 8000,
+  async fetch() {
+    const headers = new Headers({ "Content-Type": "text/html; charset=utf-8" })
+    const body = await Mizu.render(`<div *text="foo"></div>`, { context: { foo: "🌊 Yaa, mizu!" } })
+    return new Response(body, { headers })
+  },
+})
+
+console.log("Server is listening")
