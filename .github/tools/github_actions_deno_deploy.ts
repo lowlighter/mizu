7a03e8f9ac8bae70cc937b6505fdc9fa74d5bab7
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-12-16 05:40:44 -0500

    fix: builder on deno deploy (#51)

--- .github/tools/github_actions_deno_deploy.ts
+++ .github/tools/github_actions_deno_deploy.ts
@@ -2,6 +2,17 @@
 import * as JSONC from "@std/jsonc"
 import * as core from "@actions/core"
 
+// Ensure script is run in CI environment
+if (!Deno.env.get("CI")) {
+  throw new Error("This script should be run only in CI environment")
+}
+
+// Remove lockfiles and package.json to avoid resolution conflicts
+// https://github.com/denoland/deno/issues/27380
+await Deno.remove("deno.lock", { recursive: true })
+await Deno.remove("package.json", { recursive: true })
+await Deno.remove("package-lock.json", { recursive: true })
+
 // Parse deno.jsonc and print deno deploy flags
 // deno-lint-ignore no-explicit-any
 const config = JSONC.parse(await Deno.readTextFile("./deno.jsonc")) as any
