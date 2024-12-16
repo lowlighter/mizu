884d0364dfd23080ba2a6f8bebd4e40260e58c08
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-12-15 23:40:58 -0500

    continue

---
+++ .github/tools/github_actions_deno_deploy.ts
@@ -0,0 +1,12 @@
+// Imports
+import * as JSONC from "@std/jsonc"
+import * as core from "@actions/core"
+import { encodeBase64 } from "@std/encoding"
+
+// Parse deno.jsonc and print deno deploy flags
+const config = JSONC.parse(await Deno.readTextFile("./deno.jsonc")) as any
+core.setOutput("project", config.deploy.project)
+core.setOutput("entrypoint", config.deploy.entrypoint)
+core.setOutput("include", config.deploy.include.join(","))
+core.setOutput("exclude", config.deploy.exclude.join(","))
+core.setOutput("import_map", new URL(`data:application/json;base64,${encodeBase64(JSON.stringify(config.imports))}`).href)
