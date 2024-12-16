af6f30f39b5df5dc90d033ca43f7cf144fa6c439
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-12-15 23:48:21 -0500

    continue

--- .github/tools/github_actions_deno_deploy.ts
+++ .github/tools/github_actions_deno_deploy.ts
@@ -1,7 +1,6 @@
 // Imports
 import * as JSONC from "@std/jsonc"
 import * as core from "@actions/core"
-import { encodeBase64 } from "@std/encoding"
 
 // Parse deno.jsonc and print deno deploy flags
 // deno-lint-ignore no-explicit-any
@@ -10,4 +9,6 @@ core.setOutput("project", config.deploy.project)
 core.setOutput("entrypoint", config.deploy.entrypoint)
 core.setOutput("include", config.deploy.include.join(","))
 core.setOutput("exclude", config.deploy.exclude.join(","))
-core.setOutput("import_map", new URL(`data:application/json;base64,${encodeBase64(JSON.stringify(config.imports))}`).href)
+core.setOutput("import_map", ".imports_map.json")
+
+await Deno.writeTextFile(".imports_map.json", JSON.stringify({ imports: config.imports }))
