1abff987ef30a7931593bcbdf51f9e0e4a6c6c9a
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-12-16 19:19:37 -0500

    fix: commands

--- .github/tools/expand_glob.ts
+++ .github/tools/expand_glob.ts
@@ -1,14 +1,19 @@
 // deno-lint-ignore-file no-console
 // Imports
 import { expandGlob } from "@std/fs"
+import { parseArgs } from "@std/cli"
 
-/*** Fill GitHub files with current existing scopes. */
+/*** Resolve globs. */
 if (import.meta.main) {
+  const { _: globs, ...flags } = parseArgs(Deno.args, { boolean: ["break"], string: ["separator"] })
   const paths = []
-  for (const glob of Deno.args) {
+  for (const glob of globs) {
     for await (const { path } of expandGlob(glob)) {
       paths.push(path)
     }
+    if ((paths.length) && (flags.break)) {
+      break
+    }
   }
-  console.log(paths.join(","))
+  console.log(paths.join(flags.separator ?? ","))
 }
