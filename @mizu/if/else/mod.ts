56b678eae3403af876177cb74abee381b4ada532
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2026-09-04 17:37:26 -0400

    feat(if): render `<template>` content in place of the template (#99)

--- @mizu/if/else/mod.ts
+++ @mizu/if/else/mod.ts
@@ -1,6 +1,6 @@
 // Imports
-import { type Directive, Phase } from "@mizu/internal/engine"
-import { _if } from "@mizu/if"
+import { type Directive, type Nullable, Phase } from "@mizu/internal/engine"
+import { _if, type Cache } from "@mizu/if"
 export type * from "@mizu/internal/engine"
 
 /** `*else` directive. */
@@ -9,7 +9,8 @@ export const _else = {
   phase: Phase.TOGGLE,
   default: "true",
   execute(renderer, element, { attributes: [attribute] }) {
-    let previous = element.previousSibling as HTMLElement
+    const cache = renderer.cache<Cache>(_if.name)
+    let previous = element.previousSibling as Nullable<HTMLElement | Comment>
     while (previous) {
       // Break on non-empty text nodes
       if ((previous.nodeType === renderer.window.Node.TEXT_NODE) && (previous.textContent?.trim())) {
@@ -17,10 +18,14 @@ export const _else = {
       }
 
       // Force directive to `false` when a previous operand is truthy
-      if ((renderer.isHtmlElement(previous))) {
-        if (renderer.getAttributes(previous, [_if.name, _else.name] as string[], { first: true })) {
-          return _if.execute(renderer, element, { ...arguments[2], _directive: { directive: this.name, expression: attribute.value, value: "false" } })
-        }
+      const closing = cache?.templates.get(cache.generated.get(previous)!)?.end === previous
+      const opened = Boolean(cache?.templates.get(previous as Comment)?.end.parentNode)
+      if (closing || opened || ((renderer.isHtmlElement(previous)) && (renderer.getAttributes(previous, [_if.name, _else.name] as string[], { first: true })))) {
+        return _if.execute(renderer, element, { ...arguments[2], _directive: { directive: this.name, expression: attribute.value, value: "false" } })
+      }
+
+      // Break on other elements
+      if (renderer.isHtmlElement(previous)) {
         break
       }
 
@@ -28,7 +33,7 @@ export const _else = {
       if ((renderer.isComment(previous)) && (renderer.getAttributes(renderer.cache("*").get(previous), _if.name, { first: true }))) {
         return _if.execute(renderer, element, { ...arguments[2], _directive: { directive: this.name, expression: attribute.value, value: attribute.value || this.default } })
       }
-      previous = (previous as HTMLElement).previousSibling as HTMLElement
+      previous = previous.previousSibling as Nullable<HTMLElement | Comment>
     }
     renderer.warn(`[${this.name}] must be immediately preceded by another [${_if.name}] or [${_else.name}], ignoring`, element)
   },
